54f054558de382b100f22d8389f109a163191bec
Michael-Meeus <32435351+Michael-Meeus@users.noreply.github.com>
2018-10-20 17:36:23 +0200

    Oef 5, 6, 7 en half 8 maar hopelijk 2 niet

--- Chapter2_WPF_Controls/Exercise6/MainWindow.xaml.cs
+++ Chapter2_WPF_Controls/Exercise6/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 
@@ -9,6 +11,18 @@ namespace Exercise6
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Button srcButton = e.Source as Button;
+            numberTextBox.AppendText((string)srcButton.Content); 
+
+        }
+
+        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.Text[0]) || !char.IsControl(e.Text[0]);
         }
     }
 }
